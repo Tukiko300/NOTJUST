4bf88741d92e240e4aef9ed50e0770588e2117e3
Tukiko300 <105240267+Tukiko300@users.noreply.github.com>
2022-06-24 14:03:53 -0400

    Update gamesmenu.js

--- plugins/gamesmenu.js
+++ plugins/gamesmenu.js
@@ -44,14 +44,3 @@ let menu =`
 🎮𝐝𝐨𝐱𝐱𝐞𝐨 *𝐧𝐨𝐦𝐛𝐫𝐞 / @𝐭𝐚𝐠*
 ╚════════════════╝
 »𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐄 𝐉𝐔𝐄𝐆𝐎𝐒 𝐏𝐀𝐑𝐀 𝐔𝐒𝐎 𝐆𝐄𝐍𝐄𝐑𝐀𝐋. 𝐏𝐎𝐑 𝐅𝐀𝐕𝐎𝐑 𝐄𝐕𝐈𝐓𝐀𝐑 𝐄𝐋 𝐒𝐏𝐀𝐌«
-`.trim()
-let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '©𝐉𝐡𝐮𝐬𝐳ᙆ  ᷦ×͜×', '𝕄𝔼ℕ𝕌 ℙℝ𝕀ℕℂ𝕀ℙ𝔸𝕃 ', `#menu`, '𝕐𝕆𝕌𝕋𝕌𝔹𝔼', `#ytowner`, '𝕀ℕ𝕊𝕋𝔸𝔾ℝ𝔸𝕄', `#igowner`, m, false, { contextInfo: { mentionedJid }})   
-//await await await await await await conn.sendFile(m.chat, vn, 'mariana.mp3', null, m, true, {
-//type: 'audioMessage', 
-//ptt: true 
-//})
-}
-handler.command = /^(gamesmenu|menujuegos|gamesm|gsm)$/i
-handler.fail = null
-module.exports = handler
